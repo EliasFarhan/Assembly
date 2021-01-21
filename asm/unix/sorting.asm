abcfbb4dccdd304db65b4386759edfd1c91848df
efarhan <elias.farhan@gmail.com>
2021-01-21 17:32:29 +0100

    Finalize the sorting benchmark

--- asm/unix/sorting.asm
+++ asm/unix/sorting.asm
@@ -1,37 +1,48 @@
 ; Variables
 ; r10 = begin + length - 1
-; r13 = swap_count
+; rax = swap_count
 ; r11 = index ptr
 ; r14d and r15d = compared values.
 ; rdi = begin
 ; rsi = length
 global bubble_sort_asm
 bubble_sort_asm: ; (int*, size_t)
+    push r10
+    push r11
+    push r14
+    push r15
+    push rax
     lea r10, [rdi+(rsi-1)*4] ; back (end-1)
     test rsi, rsi ; count zero, bye
     je .bubble_end
 .bubble_loop_begin:
-    xor r13, r13 ; swap_count set to 0
+    xor rax, rax ; swap_count set to 0
     mov r11, rdi ; begin index ptr
 .bubble_iteration_begin:
-    cmp r11, r10 ; if we are at the end of the array
+    cmp r11, r10 ; if we are at the back of the array
     je .bubble_ending_loop
-    mov r14d, dword [r11]
-    mov r15d, dword [r11+4]
+    mov r14d, DWORD [r11]
+    mov r15d, DWORD [r11+4]
     cmp r14d, r15d
     jle .bubble_iteration_end
     ; swapping
-    mov dword [r11], r15d
-    mov dword [r11+4], r14d
-    inc r13 ; update swap_count
+    mov DWORD [r11], r15d
+    mov DWORD [r11+4], r14d
+    inc rax ; update swap_count
 .bubble_iteration_end:
     add r11, 4
     jmp .bubble_iteration_begin
 .bubble_ending_loop:
-    test  r13, r13 ; no swap means sorted
+    test  rax, rax ; no swap means sorted
     je .bubble_end
     jmp .bubble_loop_begin
 .bubble_end:
+    xor rax, rax
+    pop rax
+    pop r15
+    pop r14
+    pop r11
+    pop r10
     ret
 
 ; r10 = begin + length - 1
@@ -41,6 +52,11 @@ bubble_sort_asm: ; (int*, size_t)
 ; r12 = ptr going back
 global insertion_sort_asm
 insertion_sort_asm:
+     push r10
+    push r11
+    push r14
+    push r15
+    push r12
     lea r10, [rdi+rsi*4] ; end
     test rsi, rsi ; count zero, bye
     je .insertion_end
@@ -64,4 +80,10 @@ insertion_sort_asm:
     add r11, 4
     jmp .insertion_loop_begin
 .insertion_end:
+    xor rax, rax
+    pop r12
+    pop r15
+    pop r14
+    pop r11
+    pop r10
     ret
--- bench/bench_sorting.cpp
+++ bench/bench_sorting.cpp
@@ -8,6 +8,10 @@
 #include <random>
 #include <iostream>
 
+const size_t min_value = 8;
+const size_t max_value = 8<<10;
+
+
 static void BM_BubbleSortAsm(benchmark::State& state) 
 {
     std::vector<int> numbers(state.range(0));
@@ -25,10 +29,98 @@ static void BM_BubbleSortAsm(benchmark::State& state)
         } while(std::equal(shuffle_vector.begin(), shuffle_vector.end(), numbers.begin(), numbers.end()));
         state.ResumeTiming();
         bubble_sort_asm(shuffle_vector.data(), shuffle_vector.size());
-        
-        std::cout << "Value\n";
+
     }
 }
-BENCHMARK(BM_BubbleSortAsm)->Range(8, 128);
+BENCHMARK(BM_BubbleSortAsm)->Range(min_value, max_value);
+
+static void BM_BubbleSortCpp(benchmark::State& state)
+{
+    std::vector<int> numbers(state.range(0));
+    std::iota(numbers.begin(), numbers.end(), 0);
+    std::random_device rd;
+    std::mt19937 g(rd());
+    for (auto _ : state)
+    {
+        state.PauseTiming();
+        auto shuffle_vector = numbers;
+
+        do
+        {
+            std::shuffle(shuffle_vector.begin(), shuffle_vector.end(), g);
+        } while(std::equal(shuffle_vector.begin(), shuffle_vector.end(), numbers.begin(), numbers.end()));
+        state.ResumeTiming();
+        bubble_sort(shuffle_vector.data(), shuffle_vector.size());
+
+    }
+}
+BENCHMARK(BM_BubbleSortCpp)->Range(min_value, max_value);
+
+
+static void BM_InsertionSortAsm(benchmark::State& state)
+{
+    std::vector<int> numbers(state.range(0));
+    std::iota(numbers.begin(), numbers.end(), 0);
+    std::random_device rd;
+    std::mt19937 g(rd());
+    for (auto _ : state)
+    {
+        state.PauseTiming();
+        auto shuffle_vector = numbers;
+
+        do
+        {
+            std::shuffle(shuffle_vector.begin(), shuffle_vector.end(), g);
+        } while(std::equal(shuffle_vector.begin(), shuffle_vector.end(), numbers.begin(), numbers.end()));
+        state.ResumeTiming();
+        insertion_sort_asm(shuffle_vector.data(), shuffle_vector.size());
+
+    }
+}
+BENCHMARK(BM_InsertionSortAsm)->Range(min_value, max_value);
+
+static void BM_InsertionSortCpp(benchmark::State& state)
+{
+    std::vector<int> numbers(state.range(0));
+    std::iota(numbers.begin(), numbers.end(), 0);
+    std::random_device rd;
+    std::mt19937 g(rd());
+    for (auto _ : state)
+    {
+        state.PauseTiming();
+        auto shuffle_vector = numbers;
+
+        do
+        {
+            std::shuffle(shuffle_vector.begin(), shuffle_vector.end(), g);
+        } while(std::equal(shuffle_vector.begin(), shuffle_vector.end(), numbers.begin(), numbers.end()));
+        state.ResumeTiming();
+        insertion_sort(shuffle_vector.data(), shuffle_vector.size());
+
+    }
+}
+BENCHMARK(BM_InsertionSortCpp)->Range(min_value, max_value);
+
+static void BM_SortCpp(benchmark::State& state)
+{
+    std::vector<int> numbers(state.range(0));
+    std::iota(numbers.begin(), numbers.end(), 0);
+    std::random_device rd;
+    std::mt19937 g(rd());
+    for (auto _ : state)
+    {
+        state.PauseTiming();
+        auto shuffle_vector = numbers;
+
+        do
+        {
+            std::shuffle(shuffle_vector.begin(), shuffle_vector.end(), g);
+        } while(std::equal(shuffle_vector.begin(), shuffle_vector.end(), numbers.begin(), numbers.end()));
+        state.ResumeTiming();
+        std::sort(shuffle_vector.begin(), shuffle_vector.end());
+
+    }
+}
+BENCHMARK(BM_SortCpp)->Range(min_value, max_value);
 // Run the benchmark
 BENCHMARK_MAIN();
